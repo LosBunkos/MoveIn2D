afef1430ec6e660417903a2a4722228e8c06daa5
LosBunkos <nadavvadan@gmail.com>
2016-07-21 09:31:13 +0300

    Works except for gui.

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-/*
-Create a 2D array that looks like this:
-[
-    [ 0, 0, 0, 0 ],
-    [ 0, 0, 0, 0 ],
-    [ 0, 0, 0, 0 ],
-    [ 0, 0, 0, 1 ]
-]
-and create four functions:
-
-goUp()
-goDown()
-goLeft()
-goRight()
-
-which moves the "1" around in the table one space at a time
-
-Bonus:
-
---Make this work for a 2D array of any dimensions (not just 4x4)
---Allow the move functions to take in a parameter to move the 1 more than one spot at a time
---Combine all the move functions to be one `Move(direction, numSteps)` function
---Create a condition that doesn't allow going outside of the table bounds
-
-**Ultra Bonus**:    Visualize the table on an html page (format as you please), 
-and move the 1 around using keystrokes (W: up, A: left, S: down, D: right)
-*/
-
 var newBoard = function(y, x) {
   if (typeof x === 'undefined') {
     x = y;
   }
-
   var temp = [];
   var temp2 = [];
   for (var i = 0; i < y; i++) {
@@ -69,12 +40,12 @@ var safelyMoveTo = function (board, curr, dy, dx, functionAfter) {
     board[newY][newX] = 1;
     console.log("Moved to: {" + newX + ',' + newY + '}');
   }
-  if(!(typeof functionAfter === 'undefined')) {
-    functionAfter();
+  if(typeof functionAfter !== 'undefined') {
+    functionAfter(newX, newY);
   }
 }
 
-var go = function(direction, amount, board) {
+var go = function(direction, amount, oard, functionAfter) {
   if (typeof amount === 'undefined') {
     console.log('setting amount to default (1)')
     var amount = 1;
@@ -85,13 +56,13 @@ var go = function(direction, amount, board) {
     curr = findOne(board);
     board[curr.y][curr.x] = 0;
     if (direction === 'up') {
-      safelyMoveTo(board, curr, - 1 * amount, 0);
+      safelyMoveTo(board, curr, - 1 * amount, 0, functionAfter);
     } else if (direction === 'down') {
-        safelyMoveTo(board, curr, amount, 0);
+        safelyMoveTo(board, curr, amount, 0, functionAfter);
     } else if (direction === 'left') {
-        safelyMoveTo(board, curr, 0, -1 * amount);
+        safelyMoveTo(board, curr, 0, -1 * amount, functionAfter);
     } else if (direction === 'right') {
-        safelyMoveTo(board, curr, 0, amount);
+        safelyMoveTo(board, curr, 0, amount, functionAfter);
     } else {
        console.log("Can't go there! ("+ direction + ").");
        board[curr.y][curr.x] = 1; 
@@ -105,16 +76,23 @@ var go = function(direction, amount, board) {
 
 
 // Tests for a 4x4 board
-go("up", 1, Board);
-go("down", 1, Board);
-go('left', 4, Board);
-go('right', 1, Board)
-locationAfter = {x : 3, y : 3};
+// go("up", 1, Board);
+// go("down", 1, Board);
+// go('left', 4, Board);
+// go('right', 1, Board)
+// locationAfter = {x : 3, y : 3};
 
-if(findOne(Board).x === locationAfter.x &&
-   findOne(Board).y === locationAfter.y) {
-  console.log('Tests succeeded!');
-} else {
-  console.log('Tests failed!');
+// if(findOne(Board).x === locationAfter.x &&
+//    findOne(Board).y === locationAfter.y) {
+//   console.log('Tests succeeded!');
+// } else {
+//   console.log('Tests failed!');
+// }
+
+var current = {
+  x : 1,
+  y : 1
 }
-
+a = newBoard(5,5);
+generateBoard(a);
+updateScreen(1,1);
--- ui.js
+++ ui.js
@@ -5,8 +5,29 @@ keyToDirection = {
   d : 'right'
 }
 
+updateScreen = function(x, y) {
+  $('div .cell').css('background-color', 'red');
+  $('div #' + y + x).css('background-color', 'blue');
+}
+
+generateBoard = function(board) {
+  for(var i = 0; i < board.length; i++) {
+    $('#board').append('<div class="row">');
+    for(var j = 0; j < board[i].length; j++) {
+      $('#board').append('<div class="cell" style="width: '
+         + 100/board[i].length + '%" id="' + i + j + '"></div>');
+    }
+  }
+$('#44').css('background-color', 'blue');
+}
+
+updateScreen(1,1);
+
 $(document).keypress(function(event){
   move = String.fromCharCode(event.which); 
-  go(keyToDirection[move]);
+  console.log(move);
+  go(keyToDirection[move], 1, Board, updateScreen);
 });
+
+
 
