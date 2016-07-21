a03096194275b0b7fc5a39225a38dd6c3ea92cd9
LosBunkos <nadavvadan@gmail.com>
2016-07-21 11:03:26 +0300

    Rewrite: added _safelyGo()

--- main2.js
+++ main2.js
@@ -7,6 +7,8 @@ var Board = function(width, height) {
   this.width = width;
   this.height = height;
   this.board = [];
+  this.x = 0;
+  this.y = 0;
 
   // Methods
   this._generate = function() {
@@ -24,16 +26,18 @@ var Board = function(width, height) {
   // Init board, with [(0,0) (default) OR (x,y)] = 1
   this init = function(x, y) { 
     if(typeof x === 'undefined' || typeof y === 'undefined') {
-      console.log('Notice - x: was ' + x + '- now 0');
-      console.log('Notice - y: was ' + y + '- now 0');
+      console.log('Notice - x: was ' + x + '- now 0 (init())');
+      console.log('Notice - y: was ' + y + '- now 0 (init())');
       var x = 0;
       var y = 0;
     }
     this.board = this._generate(); // 'this' Might be problematic here (?)
     this.board[y][x] = 1;
+    this.x = x;
+    this.y = y;
   }
 
-  this.findOne = function() {
+  this.findOne = function() { // Remove this
     for (var i = 0; i < this.height; i++) {
       getIndex = this.board[height].indexOf(1);
       if (getIndex > -1) {
@@ -55,12 +59,48 @@ var Board = function(width, height) {
     }
 
     if(typeof directions[direction] === 'undefined') {
-      console.log("Error - Can't go " + direction);
+      console.log("Error - Can't go " + direction + '. (go())');
       return false;
     } else {
-      this._safelyGo(directions[direction]);
+      if(this._safelyGo(directions[direction])) {
+        console.log('Notice: Went ' + direction + '. (go(_safelyGo()))')
+      }
     }
   }
+
+  this._safelyGo = function(obj) {
+    var currX = this.board.x;
+    var currY = this.board.y;
+    var dx = obj.x;
+    var dy = obj.y;
+    var newX = currX + dx;
+    var newY = currY + dy;
+    var maxX = this.width - 1; // Max defined array index
+    var maxY = this.width - 1; // ^
+    var err = false;
+
+    // Check for array overflows
+    if(currX + d.x > maxX) {
+      console.log('Error - X overflow (_safelyGo())');
+      err = true;
+    }
+    if(currY + d.y > maxY) {
+      console.log('Error - Y overflow (_safelyGo())');
+      err = true;
+    }
+    // if everything ok, proceed with moving the 1.
+    if(!err) {
+      console.log('Notice: went success (_safelyGo())');
+      this.x = newX;
+      this.y = newY;
+      board[currY][currX] = 0;
+      board[newY][newX] = 1;
+    }
+
+    return !err;
+  }
+
+
 }
 
 
