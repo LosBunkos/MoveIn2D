a3a98760eee793c0e8e646b43730461eeed0e390
LosBunkos <nadavvadan@gmail.com>
2016-07-24 16:37:05 +0300

    solved safari border bug

--- ui.js
+++ ui.js
@@ -26,7 +26,7 @@ renderBoard = function() {
 
 renderChanges = function() {
   var $current = $('[data-x="' + board.x + '"][data-y="' + board.y + '"]').css('background-color', "black");
-  var $previous = $('[data-x="' + board.prevX + '"][data-y="' + board.prevY + '"]').css('background-color', "white");
+  var $previous = $('[data-x="' + board.prevX + '"][data-y="' + board.prevY + '"]').css('background-color', "grey");
 }
 
 handleClicks = function() {
